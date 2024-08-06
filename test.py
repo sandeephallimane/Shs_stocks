c2a4d4736e5b15f0eefaca04a43d64b239c5424c
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-06 23:15:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,28 +10,7 @@ from sklearn.model_selection import train_test_split
 import optuna
 import matplotlib.pyplot as plt
 
-script_name= 'TCS.NS'
-study_name = script_name + '_study'
-storage = 'sqlite:///' + script_name + '_study.db'
 
-data = yf.download(script_name, period='5y')['Close'].dropna()
-print(data)
-
-# Scale data
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
-
-# Define model creation function
-def create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size):
-    model = Sequential()
-    model.add(Bidirectional(LSTM(int(lstm_units), return_sequences=True, input_shape=(window_size, 1))))
-    model.add(Dropout(dropout_rate))
-    model.add(Bidirectional(GRU(int(gru_units), return_sequences=True)))
-    model.add(Dense(1))
-    model.compile(optimizer=['adam', 'rmsprop', 'sgd'][int(optimizer_idx)], loss='mean_squared_error')
-    return model
-
-# Define optimization function
 def optimize_model(trial):
     lstm_units = trial.suggest_int('lstm_units', 50, 200)
     gru_units = trial.suggest_int('gru_units', 20, 200)
@@ -58,49 +37,52 @@ def optimize_model(trial):
     mape = np.mean(np.abs((y_test - y_pred) / y_test)) * 100 if y_test.all() != 0 else 100
     r2 = 1 - (np.sum((y_test - y_pred) ** 2) / np.sum((y_test - np.mean(y_test)) ** 2))
     return mae, mse, rmse, mape, r2
+    
+def create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size):
+    model = Sequential()
+    model.add(Bidirectional(LSTM(int(lstm_units), return_sequences=True, input_shape=(window_size, 1))))
+    model.add(Dropout(dropout_rate))
+    model.add(Bidirectional(GRU(int(gru_units), return_sequences=True)))
+    model.add(Dense(1))
+    model.compile(optimizer=['adam', 'rmsprop', 'sgd'][int(optimizer_idx)], loss='mean_squared_error')
+    return model
 
-# Create study with parallelization
-study = optuna.create_study(directions=['minimize', 'minimize', 'minimize', 'minimize', 'maximize'],
+tickers = ['TCS.NS','INFY.NS']
+
+def new_lstm(ti):
+  optuna.delete_studies()
+  script_name= ti
+  study_name = script_name + '_study'
+  storage = 'sqlite:///' + script_name + '_study.db'
+  data = yf.download(script_name, period='5y')['Close'].dropna()
+  scaler = MinMaxScaler()
+  scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
+  n_jobs = -1
+  study = optuna.create_study(directions=['minimize', 'minimize', 'minimize', 'minimize', 'maximize'],
                             study_name=study_name,
                             storage=storage,
                             load_if_exists=True)
+  study.optimize(optimize_model, n_trials=50, n_jobs=n_jobs)
+  best_trials = study.best_trials
+  best_trial = best_trials[0]
+  best_model = create_model(**best_trial.params)
+  X, y = [], []
+  for i in range(len(scaled_data) - int(best_trial.params['window_size'])):
+     X.append(scaled_data[i:i + int(best_trial.params['window_size'])])
+     y.append(scaled_data[i + int(best_trial.params['window_size'])])
+  X, y = np.array(X), np.array(y)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+  best_model.fit(X_train, y_train, epochs=50, batch_size=int(best_trial.params['batch_size']), validation_split=0.2, verbose=0)
+  forecasted = []
+  current_data = scaled_data[-int(best_trial.params['window_size']):]
+  for _ in range(126):
+     prediction = best_model.predict(current_data.reshape(1, int(best_trial.params['window_size']), 1))[:, -1, :]
+     forecasted.append(prediction[0, 0])
+     current_data = np.append(current_data[1:], prediction[0, 0])
+  forecasted_prices = scaler.inverse_transform(np.array(forecasted).reshape(-1, 1))
+  return forecasted_prices
 
-n_jobs = -1
-
-# Optimize study in parallel
-study.optimize(optimize_model, n_trials=50, n_jobs=n_jobs)
-
-# Get best trials
-best_trials = study.best_trials
-
-# Train best model
-best_trial = best_trials[0]
-best_model = create_model(**best_trial.params)
-X, y = [], []
-for i in range(len(scaled_data) - int(best_trial.params['window_size'])):
-    X.append(scaled_data[i:i + int(best_trial.params['window_size'])])
-    y.append(scaled_data[i + int(best_trial.params['window_size'])])
-X, y = np.array(X), np.array(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-best_model.fit(X_train, y_train, epochs=50, batch_size=int(best_trial.params['batch_size']), validation_split=0.2, verbose=0)
-
-# Forecast
-forecasted = []
-current_data = scaled_data[-int(best_trial.params['window_size']):]
-for _ in range(126):
-    prediction = best_model.predict(current_data.reshape(1, int(best_trial.params['window_size']), 1))[:, -1, :]
-    forecasted.append(prediction[0, 0])
-    current_data = np.append(current_data[1:], prediction[0, 0])
-
-# Plot forecasted prices
-forecasted_prices = scaler.inverse_transform(np.array(forecasted).reshape(-1, 1))
-print("forecasted_prices:",forecasted_prices)
-plt.figure(figsize=(14, 7))
-plt.plot(data.index, data.values, label='Historical Data')
-forecast_dates = pd.date_range(start=data.index[-1] + pd.Timedelta(days=1), periods=126)
-plt.plot(forecast_dates, forecasted_prices, label='Forecasted Data', color='red')
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.title('TCS.NS Stock Price Forecast')
-plt.legend()
-plt.savefig('plot.png')
+for t in tickers:
+    f=new_lstm(t)
+    print("Stock name:",t)
+    print("forecasted_prices:",f)
