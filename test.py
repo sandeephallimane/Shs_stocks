2edacc405ab57ef4dd669433c3bd23565c3b89ef
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-06 16:30:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def optimize_model(trial):
     X, y = np.array(X), np.array(y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = create_model(lstm_units, gru_units, dropout_rate, optimizer_idx)
+    model = create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size)
     early_stopping = EarlyStopping(monitor='val_loss', patience=5)
     history = model.fit(X_train, y_train, epochs=50, batch_size=int(batch_size), validation_split=0.2, callbacks=[early_stopping], verbose=0)
 
