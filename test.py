8aad1964c649e6519dda3a6ca8448994a2b48fb9
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-07 22:34:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -26,60 +26,62 @@ def new_lstm(ti):
     storage = 'sqlite:///' + script_name + '_study.db'
 
     def optimize_model(trial):
-        lstm_units = trial.suggest_int('lstm_units', 50, 200)
-        gru_units = trial.suggest_int('gru_units', 20, 200)
-        dropout_rate = trial.suggest_float('dropout_rate', 0.1, 0.5)
-        batch_size = trial.suggest_int('batch_size', 32, 128)
-        optimizer_idx = trial.suggest_int('optimizer_idx', 0, 2)
-        window_size = trial.suggest_int('window_size', 100, 200)
+      lstm_units = trial.suggest_int('lstm_units', 50, 200)
+      gru_units = trial.suggest_int('gru_units', 20, 200)
+      dropout_rate = trial.suggest_float('dropout_rate', 0.1, 0.5)
+      batch_size = trial.suggest_int('batch_size', 32, 128)
+      optimizer_idx = trial.suggest_int('optimizer_idx', 0, 2)
+      window_size = trial.suggest_int('window_size', 100, 200)
 
-        X, y = [], []
-        for i in range(len(scaled_data) - int(window_size)):
-            X.append(scaled_data[i:i + int(window_size)])
-            y.append(scaled_data[i + int(window_size)])
-        X, y = np.array(X), np.array(y)
-        tscv = TimeSeriesSplit(n_splits=5)
+      X, y = [], []
+      for i in range(len(scaled_data) - int(window_size)):
+          X.append(scaled_data[i:i + int(window_size)])
+          y.append(scaled_data[i + int(window_size)])
+      X, y = np.array(X), np.array(y)
+      tscv = TimeSeriesSplit(n_splits=5)
 
-        cv_scores = []
-        for train_index, val_index in tscv.split(X):
-            X_train, X_val = X[train_index], X[val_index]
-            y_train, y_val = y[train_index], y[val_index]
+      cv_scores = []
+      for train_index, val_index in tscv.split(X):
+         X_train, X_val = X[train_index], X[val_index]
+         y_train, y_val = y[train_index], y[val_index]
+         model = create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size)
+         early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+         history = model.fit(X_train, y_train, epochs=50, batch_size=int(batch_size), validation_split=0.2, callbacks=[early_stopping], verbose=0)
+         y_pred = model.predict(X_val)[:, -1, :]
+         scores = evaluate_model(y_val, y_pred)
+         cv_scores.append(scores)
 
-            model = create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size)
-            early_stopping = EarlyStopping(monitor='val_loss', patience=5)
-            history = model.fit(X_train, y_train, epochs=50, batch_size=int(batch_size), validation_split=0.2, callbacks=[early_stopping], verbose=0)
+      avg_scores = np.mean(cv_scores, axis=0)
+      metrics = {
+        'mae': avg_scores[0],
+        'mse': avg_scores[1],
+        'rmse': avg_scores[2],
+        'mape': avg_scores[3],
+        'r2': avg_scores[4],
+        'ic': avg_scores[5],
+        'msle': avg_scores[6],
+        'mfe': avg_scores[7],
+        'mad': avg_scores[8],
+        'aic': avg_scores[9]
+             }
+    return metrics
 
-            y_pred = model.predict(X_val)[:, -1, :]
-            mae = np.mean(np.abs(y_val - y_pred))
-            mse = np.mean((y_val[:, -1] - y_pred[:, -1]) ** 2)
-            rmse = np.sqrt(mse)
-            mape = np.mean(np.abs((y_val - y_pred) / y_val)) * 100 if y_val.all() != 0 else 100
-            r2 = 1 - (np.sum((y_val - y_pred) ** 2) / np.sum((y_val - np.mean(y_val)) ** 2))
-            if y_val.shape[1] > 1 and y_pred.shape[1] > 1:
-               ic = np.corrcoef(y_val[:, -1, :], y_pred[:, -1, :])[0, 1]
-            else:
-               ic = 0
-            msle = np.mean((np.log(y_val + 1) - np.log(y_pred + 1)) ** 2)
-            mfe = np.mean(y_val - y_pred)
-            mad = np.mean(np.abs(y_val - y_pred))
-            aic = 2 * (len(model.layers) + 1) - 2 * np.log(mse)
-
-            cv_scores.append([mae, mse, rmse, mape, r2, ic, msle, mfe, mad, aic])
-
-        avg_scores = np.mean(cv_scores, axis=0)
-        metrics = {
-           'mae': avg_scores[0],
-           'mse': avg_scores[1],
-           'rmse': avg_scores[2],
-           'mape': avg_scores[3],
-           'r2': avg_scores[4],
-           'ic': avg_scores[5],
-           'msle': avg_scores[6],
-           'mfe': avg_scores[7],
-           'mad': avg_scores[8],
-           'aic': avg_scores[9]
-                 }
-        return metrics
+    def evaluate_model(y_val, y_pred):
+        mae = np.mean(np.abs(y_val - y_pred))
+        mse = np.mean((y_val[:, -1] - y_pred[:, -1]) ** 2)
+        rmse = np.sqrt(mse)
+        mape = np.mean(np.abs((y_val - y_pred) / y_val)) * 100 if y_val.all() != 0 else 100
+        r2 = 1 - (np.sum((y_val - y_pred) ** 2) / np.sum((y_val - np.mean(y_val)) ** 2))
+        if y_val.shape[1] > 1 and y_pred.shape[1] > 1:
+            ic = np.corrcoef(y_val[:, -1, :], y_pred[:, -1, :])[0, 1]
+        else:
+            ic = 0.0
+        msle = np.mean((np.log(y_val + 1) - np.log(y_pred + 1)) ** 2)
+        mfe = np.mean(y_val - y_pred)
+        mad = np.mean(np.abs(y_val - y_pred))
+        aic = 2 * (len(model.layers) + 1) - 2 * np.log(mse)
+    return [mae, mse, rmse, mape, r2, ic, msle, mfe, mad, aic]
+    
     def create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size):
         model = Sequential()
         model.add(Bidirectional(LSTM(int(lstm_units), return_sequences=True, input_shape=(window_size, 1))))
