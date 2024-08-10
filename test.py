0f56305e754ac5f04c44738e07ad2ceea7f48ea7
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-10 15:39:06 +0530

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ if physical_devices:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 tickers = ['TCS.NS','INFY.NS']
-early_stopping = EarlyStopping(monitor='val_loss', patience=5 )
+early_stopping = EarlyStopping(monitor='mean_squared_error', patience=5 )
 
 def stk_dt(tk,scaler):
    data = yf.download(tk, period='5y')['Close'].dropna()
