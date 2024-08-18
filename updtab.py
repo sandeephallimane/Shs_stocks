f49ed69b8fafcde773ce979251bcd2ceb0ffc14e
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-18 10:18:19 +0530

    Update updtab.py

--- updtab.py
+++ updtab.py
@@ -127,7 +127,7 @@ def optimize_model(trial, scaled_data, lf):
     X_train, X_val_test, y_train, y_val_test = train_test_split(X, y, test_size=0.2, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
     model = create_model(lstm_units, gru_units, dropout_rate, optimizer_idx, batch_size, window_size, activation, lf)
-    history = model.fit(X_train, y_train, epochs=20, batch_size=int(batch_size), callbacks=[early_stopping], verbose=0)
+    history = model.fit(X_train, y_train, epochs=25, batch_size=int(batch_size), callbacks=[early_stopping], verbose=0)
     mae = history.history['val_mean_absolute_error'][-1]
     mse = history.history['val_mean_squared_error'][-1]
     mape = history.history['val_mean_absolute_percentage_error'][-1]
@@ -145,7 +145,7 @@ def new_lstm(ti,data,cmp):
     lf= select_loss_function(scaled_data)
     sampler = TPESampler()   #RandomSampler(),GridSampler() 
     study = optuna.create_study(directions=['minimize', 'minimize','minimize','minimize', 'minimize'], study_name=study_name, storage=storage, load_if_exists=True, sampler=sampler)
-    study.optimize(lambda trial: optimize_model(trial, scaled_data, lf), n_trials=5, n_jobs=8)
+    study.optimize(lambda trial: optimize_model(trial, scaled_data, lf), n_trials=100, n_jobs=8)
     best_trials = study.best_trials
     best_trial = best_trials[0]  
     best_model = create_model(**best_trial.params, loss_function=lf)
