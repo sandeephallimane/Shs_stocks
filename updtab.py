62259592f98c10a62eee56842cc1c5a7c8435ed7
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-19 18:49:51 +0530

    Update updtab.py

--- updtab.py
+++ updtab.py
@@ -131,54 +131,52 @@ def create_model1(trial, window_size, loss_functions):
 def create_model(trial, window_size, loss_functions):
     loss_name = trial.suggest_categorical('loss_function', loss_categories)
     loss = loss_functions_dict[loss_name]
-    print(loss)
+    
     model = Sequential()
-    model.add(Bidirectional(LSTM(int(trial.suggest_int('lstm_units', 50, 200)), 
-                                  return_sequences=True, 
-                                  input_shape=(window_size, 1), 
-                                  activation=trial.suggest_categorical('activation', ['relu', 'leaky_relu', 'swish', 'tanh']), 
-                                  dropout=trial.suggest_float('dropout_rate', 0.1, 0.5), 
-                                  recurrent_dropout=trial.suggest_float('recurrent_dropout',0.1, 0.2))))
+    model.add(LSTM(
+        int(trial.suggest_int('lstm_units', 50, 100)), 
+        input_shape=(window_size, 1), 
+        activation=trial.suggest_categorical('activation', ['relu', 'leaky_relu', 'swish', 'tanh']),
+        dropout=trial.suggest_float('dropout_rate', 0.1, 0.4),
+        recurrent_dropout=trial.suggest_float('recurrent_dropout', 0.1, 0.2)
+    ))
     model.add(BatchNormalization())
     model.add(Dropout(trial.suggest_float('dropout_rate', 0.1, 0.5)))
-    model.add(Bidirectional(GRU(int(trial.suggest_int('gru_units', 50, 200)), 
-                                  return_sequences=True, 
-                                  activation=trial.suggest_categorical('activation', ['relu', 'leaky_relu', 'swish', 'tanh']), 
-                                  dropout=trial.suggest_float('dropout_rate', 0.1, 0.5), 
-                                  recurrent_dropout=trial.suggest_float('recurrent_dropout', 0.1, 0.3))))
+    
+    model.add(GRU(
+        int(trial.suggest_int('gru_units', 50, 100)),
+        dropout=trial.suggest_float('dropout_rate', 0.1, 0.4),
+        recurrent_dropout=trial.suggest_float('recurrent_dropout', 0.1, 0.2)
+    ))
     model.add(Dropout(trial.suggest_float('dropout_rate', 0.1, 0.5)))
-    model.add(Dense(1, kernel_regularizer=l2(trial.suggest_float('l2', 0.01, 0.1))))
+    model.add(Dense(1, kernel_regularizer=l2(trial.suggest_float('l2', 0.01, 0.1)))) 
     optimizers = [Adam(), RMSprop(), SGD(), AdamW(), Nadam()]
     model.compile(
         optimizer=optimizers[trial.suggest_int('optimizer_idx', 0, 4)],
         loss=loss,
         metrics=['mean_squared_error', 'mean_absolute_error']
     )
-    
     return model
 
 def optimize_model(trial, scaled_data):
-    window_size = trial.suggest_int('window_size', 50, 200)
-    batch_size = trial.suggest_int('batch_size', 32, 128)
-    
+    window_size = trial.suggest_int('window_size', 50, 150)
+    batch_size = trial.suggest_int('batch_size', 32, 64)
     X, y = [], []
     for i in range(len(scaled_data) - int(window_size)):
         X.append(scaled_data[i:i + int(window_size)])
         y.append(scaled_data[i + int(window_size)])
     X, y = np.array(X), np.array(y)
-    
-    model = create_model(trial, window_size, loss_functions)
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+    model = create_model(trial, window_size, loss_functions) 
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
-    
-    history = model.fit(X, y, epochs=21, batch_size=int(batch_size), 
-                        validation_split=trial.suggest_categorical('validation_split', [0.2, 0.3, 0.4]), 
-                        callbacks=[early_stopping, reduce_lr], 
-                        verbose=0)
+    history = model.fit(
+        X, y, epochs=30, batch_size=int(batch_size), 
+        validation_split=trial.suggest_categorical('validation_split', [0.2, 0.3, 0.4]), 
+        callbacks=[early_stopping, reduce_lr], 
+        verbose=0 )
     mae = history.history['val_mean_absolute_error'][-1]
     mse = history.history['val_mean_squared_error'][-1]
-    return mse,mae
-
+    return mse, mae
 def new_lstm(ti, data, cmp):
     script_name = ti
     study_name = script_name + '1_study'
@@ -189,35 +187,38 @@ def new_lstm(ti, data, cmp):
     sampler = TPESampler()   
     study = create_study(directions=['minimize', 'minimize'], study_name=study_name, storage=storage, load_if_exists=True, sampler=sampler)
     study.optimize(lambda trial: optimize_model(trial, scaled_data), n_trials=100, n_jobs=8)
+    
     best_trials = study.best_trials
     best_trial = best_trials[0]  
     print("best_trial.params:", best_trial.params) 
     best_model = create_model(best_trial, int(best_trial.params['window_size']), loss_functions)
     print("best_model.summary:", best_model.summary()) 
+    
     X, y = [], []
     for i in range(len(scaled_data) - int(best_trial.params['window_size'])):
-      X.append(scaled_data[i:i + int(best_trial.params['window_size'])])
-      y.append(scaled_data[i + int(best_trial.params['window_size'])])
+        X.append(scaled_data[i:i + int(best_trial.params['window_size'])])
+        y.append(scaled_data[i + int(best_trial.params['window_size'])])
     X, y = np.array(X), np.array(y)
     best_model.fit(X, y, epochs=100, batch_size=int(best_trial.params['batch_size']), callbacks=[early_stopping], verbose=0)
+    
     forecast_period = 126
     forecasted_prices = []
     window_size = int(best_trial.params['window_size'])
     current_data = scaled_data[-window_size:]
-    forecasted_prices = []
-    current_data = scaled_data[-window_size:]
+    
     for _ in range(forecast_period):
-      current_data_reshaped = current_data.reshape(1, window_size, 1)
-      prediction = best_model.predict(current_data_reshaped)
-      forecasted_price =  prediction[0, -1, 0]
-      forecasted_prices.append(forecasted_price)
-      current_data = np.append(current_data[1:], forecasted_price)
+        current_data_reshaped = current_data.reshape(1, window_size, 1)
+        prediction = best_model.predict(current_data_reshaped)
+        forecasted_price = prediction[0, 0]  # Since we only need the last prediction
+        forecasted_prices.append(forecasted_price)
+        current_data = np.append(current_data[1:], forecasted_price)
+    
     forecasted_prices = scaler.inverse_transform(np.array(forecasted_prices).reshape(-1, 1))
     min_p = np.min(forecasted_prices).round(2)
     max_p = np.max(forecasted_prices).round(2)
     avg_p = np.mean(forecasted_prices).round(2)
     ret_p = ((avg_p-cmp)*100/cmp).round(2)
-    return min_p,max_p,avg_p,ret_p
+    return min_p, max_p, avg_p, ret_p
 
 b= download_file(url2, filename2)
 if a>b:
