1fcd06dddceff7184112a65aa4191fd4475014ed
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2024-08-22 13:37:37 +0530

    Update simpmodel.py

--- simpmodel.py
+++ simpmodel.py
@@ -210,7 +210,7 @@ def optimize_model(trial: Trial, scaled_data: np.ndarray):
     early_stopping = CustomEarlyStopping(min_epochs=30, patience=10, restore_best_weights=True)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=15, min_lr=0.001)
     
-    history = model.fit(X_train, y_train, epochs=60, batch_size=int(batch_size), validation_data=0.2, verbose=0)
+    history = model.fit(X_train, y_train, epochs=60, batch_size=int(batch_size), validation_data=(X_val_test, y_val_test), verbose=0)
     
     mape = history.history['val_mean_absolute_percentage_error'][-1]
     mse = history.history['val_mean_squared_error'][-1]
