a40fa9d858776590c45a484936843edfd553f8f1
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2025-04-24 13:16:15 +0530

    Update Beta_d_n.py

--- Beta_d_n.py
+++ Beta_d_n.py
@@ -7,7 +7,7 @@ genai.configure(api_key=ak)
 
 model = genai.GenerativeModel("models/gemini-2.0-flash")
 
-prompt = """
+prompt =  f"""
 You are a financial analyst. Summarize top finance news in India for {today_date} with headlines and 2-3 sentence summaries for each.
 Then list major stocks in focus for today along with a brief reason for their movement (e.g. earnings, news, announcements).
 
