fa9b633e6c44ee327f758dd183a237e46d3bb79d
sandeephallimane <125996211+sandeephallimane@users.noreply.github.com>
2025-04-24 13:08:40 +0530

    Update Beta_d_n.py

--- Beta_d_n.py
+++ Beta_d_n.py
@@ -1,14 +1,14 @@
-
+from datetime import datetime
 import google.generativeai as genai
 import os
-
+today_date = datetime.now().strftime("%B %d, %Y")
 ak= os.getenv('AK')
 genai.configure(api_key=ak)
 
 model = genai.GenerativeModel("models/gemini-2.0-flash")
 
 prompt = """
-You are a financial analyst. Summarize today's top finance news in India with headlines and 2-3 sentence summaries for each.
+You are a financial analyst. Summarize top finance news in India for {today_date} with headlines and 2-3 sentence summaries for each.
 Then list major stocks in focus for today along with a brief reason for their movement (e.g. earnings, news, announcements).
 
 Structure your response like this:
